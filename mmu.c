4d968ca556174330af396ae4c4134356d7b09f4b
Baron105 <barunparua@gmail.com>
2024-03-29 21:09:57 +0530

    mmu

---
+++ mmu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+#define P(s) semop(s, &pop, 1)
+#define V(s) semop(s, &vop, 1)
